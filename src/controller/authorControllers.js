b99195deb1d847ab03f99eb66d08c3fecbc524d8
dhirajuranium <dhirajpatil2311@gmail.com>
2022-04-27 15:29:23 +0530

    create author

---
+++ src/controller/authorControllers.js
@@ -0,0 +1,30 @@
+const authorModel = require('../modules/authorModule')
+
+
+const createAuthor = async function (req,res) {
+    try {const reqAuthor = req.body;
+        if(Object.keys(reqAuthor).length==0){
+            return res.status(400).send({status:false,msg:"Body is required"})
+        }
+        const saveData = await authorModel.create(reqAuthor)
+        res.status(201).send({status:true,data: saveData})
+    }
+    catch(err){
+        console.log(err.message)
+        res.status(400).send({status:false,msg: err.message})
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports.createAuthor= createAuthor
--- src/route/route.js
+++ src/route/route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const authorController = require("../controller/authorControllers")
 const blogscontroller = require("../controller/blogscontroller")
 
 router.delete("/blogs/:blogId", blogscontroller.deleteBlogsById)
 
 router.post("/blogs", blogscontroller.createBlogs)
 
+router.post("/createAuthors",authorController.createAuthor)
 
 
 
