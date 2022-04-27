4546fe02dea49e71f12cad5433875b4c4d8fac4c
Dolly <showdolly100@gmail.com>
2022-04-27 16:18:16 +0530

    queryBlogsDelete

--- src/route/route.js
+++ src/route/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const authorController = require("../controller/authorControllers")
 const blogscontroller = require("../controller/blogscontroller")
+const queryBlogsDelete = require("../controller/queryBlogsDelete")
 
 router.delete("/blogs/:blogId", blogscontroller.deleteBlogsById)
 
@@ -10,6 +11,7 @@ router.post("/blogs", blogscontroller.createBlogs)
 
 router.post("/createAuthors",authorController.createAuthor)
 
+router.delete("/blogs",queryBlogsDelete.deleteBlogsByQuery)
 
 
 
