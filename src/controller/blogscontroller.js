6b63e22cd4abacb967ad501ecfbb18c027d32504
AmitKumarSharma4046 <aerez4046@gmail.com>
2022-04-27 18:31:12 +0530

    delete by query in blogcontoller

--- src/controller/blogscontroller.js
+++ src/controller/blogscontroller.js
@@ -79,10 +79,34 @@ const deleteBlogsById = async function (req, res) {
 }
 
 
+const deleteBlogsByQuery = async function (req, res) {
+    try {
+        let data = req.query
+        if (Object.keys(data).length==0) {
+        res.status(400).send({status:false,msg:"no query params available " })
+        }
+        data.isDeleted=false
+        const available=await blogsModule.find(data).count()
+        if (available==0){
+            res.status(404).send({status:false,msg:"query data not found " })  
+        }
+        const deleteData = await blogsModule.updateMany(data, { $set: { isDeleted: true } })
+        res.status(200).send({ status: true, msg: deleteData })
+    } catch (error) {
+        res.status(500).send({status: false, msg: error.message  })
+
+    }
+
+}
 
 
 
 
-//module.exports.getAllBlogs = getAllBlogs
+
+
+
+
+
 module.exports.createBlogs = createBlogs
 module.exports.deleteBlogsById = deleteBlogsById
+module.exports.deleteBlogsByQuery = deleteBlogsByQuery
--- src/route/route.js
+++ src/route/route.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 const authorController = require("../controller/authorControllers")
 const blogscontroller = require("../controller/blogscontroller")
-const queryBlogsDelete = require("../controller/queryBlogsDelete")
 
-router.delete("/blogs/:blogId", blogscontroller.deleteBlogsById)
 
 router.post("/blogs", blogscontroller.createBlogs)
 
 router.post("/createAuthors",authorController.createAuthor)
 
-router.delete("/blogs",queryBlogsDelete.deleteBlogsByQuery)
+router.delete("/blogs/:blogId", blogscontroller.deleteBlogsById)
+
+router.delete("/blogs",blogscontroller.deleteBlogsByQuery)
 
 
 
